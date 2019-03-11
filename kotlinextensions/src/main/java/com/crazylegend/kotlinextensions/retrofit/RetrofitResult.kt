1f210b01fd24aae65d88f80bd12fda8514a2d95a
CraZyLegenD <crazyxylene@gmail.com>
2019-03-11 13:48:15 +0100

    empty data handle

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/retrofit/RetrofitResult.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/retrofit/RetrofitResult.kt
@@ -14,6 +14,7 @@ sealed class RetrofitResult<out T> {
     object Loading : RetrofitResult<Nothing>()
     object NoData : RetrofitResult<Nothing>()
     object Refreshing : RetrofitResult<Nothing>()
+    object EmptyData : RetrofitResult<Nothing>()
     data class Error(val message: String, val exception: Exception?=null, val throwable: Throwable) : RetrofitResult<Nothing>()
     data class ApiError(private val context: Context, private val responseCode: Int, private val errorBody: ResponseBody?, private val showErrorByDefault:Boolean = true) : RetrofitResult<Nothing>(){
 
