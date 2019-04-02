f5659eff219abeace77e870af87203aed7248398
CraZyLegenD <crazyxylene@gmail.com>
2019-04-02 11:28:28 +0200

    encryption util added

--- app/src/main/java/com/crazylegend/setofusefulkotlinextensions/MainActivity.kt
+++ app/src/main/java/com/crazylegend/setofusefulkotlinextensions/MainActivity.kt
@@ -4,8 +4,11 @@ package com.crazylegend.setofusefulkotlinextensions
 
 
 
+import android.app.Notification
 import android.os.Bundle
 import com.crazylegend.kotlinextensions.codestyle.BaseActivity
+import com.crazylegend.kotlinextensions.dataStructuresAndAlgorithms.cryptography.EncriptionUtil
+import com.crazylegend.kotlinextensions.log.debug
 
 
 class MainActivity : BaseActivity() {
@@ -19,8 +22,17 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+       EncriptionUtil.generateKey(this)
+        val textEncrypted = EncriptionUtil.encrypt("asdfasdfasdf")
+        val textDecrypted = EncriptionUtil.decrypt(textEncrypted)
+
+
+        textEncrypted.debug("Encrypting $textEncrypted")
+        textDecrypted.debug("Decrypting $textDecrypted")
+
 
     }
+
 
 
 
