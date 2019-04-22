a83333199342336a1f6959e8fda9dcc8704f4a09
CraZyLegenD <crazyxylene@gmail.com>
2019-04-22 10:08:23 +0200

    moshi and gson instances

---
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/retrofit/instances/GsonInstance.kt
@@ -0,0 +1,17 @@
+package com.crazylegend.kotlinextensions.retrofit.instances
+
+import com.google.gson.Gson
+
+
+/**
+ * Created by hristijan on 4/22/19 to long live and prosper !
+ */
+object GsonInstance {
+    private var gson: Gson? = null
+    fun getGsonInstance(): Gson? {
+        if (gson == null) {
+            gson = Gson()
+        }
+        return gson
+    }
+}
---
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/retrofit/instances/MoshiInstance.kt
@@ -0,0 +1,18 @@
+package com.crazylegend.kotlinextensions.retrofit.instances
+
+import com.squareup.moshi.Moshi
+
+
+/**
+ * Created by hristijan on 4/22/19 to long live and prosper !
+ */
+object MoshiInstance {
+    private var moshi: Moshi? = null
+
+    fun getMoshiInstance(): Moshi? {
+        if (moshi == null){
+            moshi = Moshi.Builder().build()
+        }
+        return moshi
+    }
+}
