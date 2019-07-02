396b814ef049c9602bcf7ae24ac4a17916a7b22e
CraZy LegenD <crazyxylene@gmail.com>
2019-07-02 22:13:36 +0200

    for item click listener default open purpose

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/recyclerview/clickListeners/forItemClickListener.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/recyclerview/clickListeners/forItemClickListener.kt
@@ -7,7 +7,7 @@ import android.view.View
  * Created by hristijan on 2/20/19 to long live and prosper !
  */
 interface forItemClickListener<T> {
-    fun forItem(position:Int, item:T, purpose:ClickPurpose, view: View)
+    fun forItem(position:Int, item:T, purpose:ClickPurpose = ClickPurpose.OPEN, view: View)
 }
 
 fun <T> forItemClickListenerDSL(callback: (position: Int, item: T, purpose: ClickPurpose, view: View) -> Unit = { _, _, _, _ -> }): forItemClickListener<T> {
