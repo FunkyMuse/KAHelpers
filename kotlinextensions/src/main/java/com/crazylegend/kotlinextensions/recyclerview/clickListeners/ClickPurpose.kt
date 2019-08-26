99ebed1f7e9a5a309e6827b7d105e6161c0de9eb
CraZyLegenD <crazyxylene@gmail.com>
2019-08-26 12:40:26 +0200

    click listeners clean up

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/recyclerview/clickListeners/ClickPurpose.kt
+++
@@ -1,9 +0,0 @@
-package com.crazylegend.kotlinextensions.recyclerview.clickListeners
-
-
-/**
- * Created by hristijan on 2/20/19 to long live and prosper !
- */
-enum class ClickPurpose {
-        OPEN, CLICK_ON_ITEM, OTHER
-}
--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/recyclerview/clickListeners/forItemClickListener.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/recyclerview/clickListeners/forItemClickListener.kt
@@ -7,13 +7,13 @@ import android.view.View
  * Created by hristijan on 2/20/19 to long live and prosper !
  */
 interface forItemClickListener<T> {
-    fun forItem(position:Int, item:T, purpose:ClickPurpose = ClickPurpose.OPEN, view: View)
+    fun forItem(position: Int, item: T, view: View)
 }
 
-fun <T> forItemClickListenerDSL(callback: (position: Int, item: T, purpose: ClickPurpose, view: View) -> Unit = { _, _, _, _ -> }): forItemClickListener<T> {
+fun <T> forItemClickListenerDSL(callback: (position: Int, item: T, view: View) -> Unit = { _, _, _ -> }): forItemClickListener<T> {
     return object : forItemClickListener<T> {
-        override fun forItem(position: Int, item: T, purpose: ClickPurpose, view: View) {
-            callback(position, item, purpose, view)
+        override fun forItem(position: Int, item: T, view: View) {
+            callback(position, item, view)
         }
     }
 }
