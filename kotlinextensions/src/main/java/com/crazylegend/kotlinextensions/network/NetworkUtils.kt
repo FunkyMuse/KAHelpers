3271b5335471b55e1ac7885feb5fc35a76c12048
CraZyLegenD <crazyxylene@gmail.com>
2019-11-04 14:58:45 +0100

    inet and ip address now separable

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/network/NetworkUtils.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/network/NetworkUtils.kt
@@ -1,7 +1,9 @@
 package com.crazylegend.kotlinextensions.network
 
 import android.content.Context
-import com.crazylegend.kotlinextensions.context.wifiManager
+import android.net.wifi.WifiManager
+import com.crazylegend.kotlinextensions.tryOrElse
+import com.crazylegend.kotlinextensions.tryOrNull
 import java.net.InetAddress
 import java.net.UnknownHostException
 
@@ -10,25 +12,26 @@ import java.net.UnknownHostException
  * Created by hristijan on 6/21/19 to long live and prosper !
  */
 
+fun Context.getIPAddress(defaultAddress: String = "127.0.0.1"): String {
+    return tryOrNull {
+        getInetAddress()?.hostAddress
+    } ?: defaultAddress
+}
+
 @Throws(UnknownHostException::class)
-fun Context.getIpAddress(): InetAddress? {
-    val wifiMgr = wifiManager
-    val wifiInfo = wifiMgr?.connectionInfo
-    val ip = wifiInfo?.ipAddress
+fun Context.getInetAddress(): InetAddress? {
+    val wifiMgr = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    val wifiInfo = wifiMgr.connectionInfo
+    val ip = wifiInfo.ipAddress
 
     return if (ip == 0) {
         null
     } else {
-        val ipAddress = ip?.let { convertIpAddress(it) }
+        val ipAddress = convertIpAddress(ip)
         InetAddress.getByAddress(ipAddress)
     }
 }
 
 private fun convertIpAddress(ip: Int): ByteArray {
-    return byteArrayOf(
-            (ip and 0xFF).toByte(),
-            (ip shr 8 and 0xFF).toByte(),
-            (ip shr 16 and 0xFF).toByte(),
-            (ip shr 24 and 0xFF).toByte()
-    )
+    return byteArrayOf((ip and 0xFF).toByte(), (ip shr 8 and 0xFF).toByte(), (ip shr 16 and 0xFF).toByte(), (ip shr 24 and 0xFF).toByte())
 }
