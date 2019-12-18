2431d6d0b2e7b4564e7ae7257d49cdc649b58ba7
CraZyLegenD <crazyxylene@gmail.com>
2019-12-18 09:31:04 +0100

    fixed fuck up not showing parcelize in the library

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/location/ObtainedLocationModel.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/location/ObtainedLocationModel.kt
@@ -1,12 +1,17 @@
 package com.crazylegend.kotlinextensions.location
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 
 /**
  * Created by crazy on 3/30/19 to long live and prosper !
  */
+
+@Parcelize
 data class ObtainedLocationModel(var address :String = "",
                                  var city :String = "",
                                  var state:String = "",
                                  var country :String = "",
                                  var postalCode :String = "",
-                                 var knownName :String = "")
+                                 var knownName: String = "") : Parcelable
