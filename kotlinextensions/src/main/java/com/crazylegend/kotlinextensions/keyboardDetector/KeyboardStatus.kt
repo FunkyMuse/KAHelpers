233123ed6b096d00e46accc7e8e724ab3deb480a
CraZyLegenD <crazyxylene@gmail.com>
2020-03-05 10:29:03 +0100

    build: kotlin update
    Also removed the keyboard detection utils since this is coming around the corner
    https://twitter.com/chrisbanes/status/1230598177511788545

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/keyboardDetector/KeyboardStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.crazylegend.kotlinextensions.keyboardDetector
-
-
-/**
- * Created by hristijan on 3/7/19 to long live and prosper !
- */
-enum class KeyboardStatus {
-    KEYBOARD_OPEN, KEYBOARD_CLOSED
-}
