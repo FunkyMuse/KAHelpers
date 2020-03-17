4bfccbddc622542061417c9e555ca4ce65f27fc4
CraZy LegenD <crazyxylene@gmail.com>
2020-03-17 09:47:50 +0100

    feat: magisk detector

---
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/security/HarmfulPackages.kt
@@ -0,0 +1,31 @@
+package com.crazylegend.kotlinextensions.security
+
+
+
+/**
+ * Created by crazy on 3/17/20 to long live and prosper !
+ */
+
+
+/**
+ * You can use [Context.isAppInstalled()] and iterate through the list
+ */
+val appModifiersList get() = listOf(
+        Pair("com.luckypatchers.luckypatcherinstaller", "Lucky patcher"),
+        Pair("ru.qQxnLnBd.UdzzaLcEz", "Cree"),
+        Pair("com.chelpus.lackypatch", "Lucky patcher"),
+        Pair("com.adguard.android", "Ad guard"),
+        Pair("com.aviraxp.adblocker.continued", "Ad blocker"),
+        Pair("cc.madkite.freedom", "Freedom"),
+        Pair("cc.cz.madkite.freedom", "Freedom"),
+        Pair("com.adguard.android.contentblocker", "Ad guard"),
+        Pair("catch_.me_.if_.you_.can_", "Game guardian"),
+        Pair("sbgamehacker.net", "SB game hacker"),
+        Pair("com.xmodgame", "Xmod game"),
+        Pair("org.blokada.alarm", "Blokada"),
+        Pair("org.adaway", "Adaway"),
+        Pair("org.adblockplus.android", "Adblock plus"),
+        Pair("com.lulu.lulubox", "Lulu box"),
+        Pair("com.topjohnwu.magisk", "Magisk"),
+        Pair("org.cheatengine.cegui", "Cheat engine"),
+        Pair("com.appsara.app", "App sara"))
