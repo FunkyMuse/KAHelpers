75af39e0bd4de6ed9a211953a1697d2c80a9fc90
CraZy LegenD <crazyxylene@gmail.com>
2020-04-30 18:20:02 +0200

    feature: more preference extensions

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/preferences/PreferencesExtensions.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/preferences/PreferencesExtensions.kt
@@ -1,15 +1,15 @@
 package com.crazylegend.kotlinextensions.preferences
 
-import androidx.preference.ListPreference
+import androidx.preference.DialogPreference
 import androidx.preference.Preference
-import androidx.preference.SwitchPreferenceCompat
+import androidx.preference.TwoStatePreference
 
 
 /**
  * Created by crazy on 4/30/20 to long live and prosper !
  */
 
-fun SwitchPreferenceCompat?.booleanChangeListener(action: (preference: Preference, newValue: Boolean) -> Unit = { _, _ -> }) {
+fun TwoStatePreference?.booleanChangeListener(action: (preference: Preference, newValue: Boolean) -> Unit = { _, _ -> }) {
     this?.setOnPreferenceChangeListener { pref, value ->
         value as Boolean
         action(pref, value)
@@ -17,9 +17,17 @@ fun SwitchPreferenceCompat?.booleanChangeListener(action: (preference: Preferenc
     }
 }
 
-fun ListPreference?.stringChangeListener(action: (preference: Preference, newValue: String) -> Unit = { _, _ -> }) {
+fun DialogPreference?.stringChangeListener(action: (preference: Preference, newValue: String) -> Unit = { _, _ -> }) {
     this?.setOnPreferenceChangeListener { pref, value ->
         value as String
+        action(pref, value)
+        true
+    }
+}
+
+fun DialogPreference?.booleanChangeListener(action: (preference: Preference, newValue: Boolean) -> Unit = { _, _ -> }) {
+    this?.setOnPreferenceChangeListener { pref, value ->
+        value as Boolean
         action(pref, value)
         true
     }
@@ -30,4 +38,30 @@ fun Preference?.onClick(action: () -> Unit = {}) {
         action()
         true
     }
+}
+
+fun Preference?.disable() {
+    this?.isEnabled = false
+}
+
+fun Preference?.enable() {
+    this?.isEnabled = true
+}
+
+fun TwoStatePreference?.disableAndUnCheck() {
+    this?.isEnabled = false
+    this?.isChecked = false
+}
+
+fun TwoStatePreference?.enableAndCheck() {
+    this?.isEnabled = true
+    this?.isChecked = true
+}
+
+fun Preference?.disableCopying() {
+    this?.isCopyingEnabled = false
+}
+
+fun Preference?.enableCopying() {
+    this?.isCopyingEnabled = true
 }
