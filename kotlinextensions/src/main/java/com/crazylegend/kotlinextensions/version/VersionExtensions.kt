817015ae7aaf8679654ca7157bf64cf55f3519d5
CraZyLegenD <crazyxylene@gmail.com>
2020-05-14 11:26:42 +0200

    refactor: api unnecessary removal

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/version/VersionExtensions.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/version/VersionExtensions.kt
@@ -123,28 +123,7 @@ fun isLollipop(): Boolean =
 
 enum class Api(val sdkCode: Int) {
 
-
-    BASE(1),
-    BASE_1_1(2),
-    CUPCAKE(3),
     CUR_DEVELOPMENT(10000),
-    DONUT(4),
-    ECLAIR(5),
-    ECLAIR_0_1(6),
-    ECLAIR_MR1(7),
-    FROYO(8),
-    GINGERBREAD(9),
-    GINGERBREAD_MR1(10),
-    HONEYCOMB(11),
-    HONEYCOMB_MR1(12),
-    HONEYCOMB_MR2(13),
-    ICE_CREAM_SANDWICH(14),
-    ICE_CREAM_SANDWICH_MR1(15),
-    JELLY_BEAN(16),
-    JELLY_BEAN_MR1(17),
-    JELLY_BEAN_MR2(18),
-    KITKAT(19),
-    KITKAT_WATCH(20),
     LOLLIPOP(21),
     LOLLIPOP_MR1(22),
     M(23),
@@ -152,7 +131,8 @@ enum class Api(val sdkCode: Int) {
     N_MR1(25),
     O(26),
     P(27),
-    Q(29)
+    Q(29),
+    R(30)
 
 
 }
