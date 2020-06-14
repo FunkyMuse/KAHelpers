4052a7cdbcbe822957a576700fb32d76faf17bbb
CraZyLegenD <crazyxylene@gmail.com>
2020-06-14 12:44:48 +0200

    refactor: palette comments

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/palette/PaletteExtensions.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/palette/PaletteExtensions.kt
@@ -14,14 +14,18 @@ import androidx.palette.graphics.Palette
  */
 val Bitmap.palette get() = Palette.from(this).generate()
 
-
-fun Bitmap.palette(paletteCallback: (palette: Palette?) -> Unit) {
+/**
+ * Creates palette async
+ * @receiver Bitmap
+ * @param paletteCallback Function<[@palette] Palette?, Unit>
+ */
+inline fun Bitmap.palette(crossinline paletteCallback: (palette: Palette?) -> Unit) {
     Palette.from(this).generate {
         paletteCallback(it)
     }
 }
 
-fun Bitmap.buildPalette(buildCallback: Palette.Builder.() -> Unit) {
+inline fun Bitmap.buildPalette(buildCallback: Palette.Builder.() -> Unit) {
     val builder = Palette.Builder(this)
     builder.buildCallback()
 }
