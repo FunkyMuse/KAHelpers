48068ded7a50eee60fcda5aa34b5204c232a412e
CraZyLegenD <crazyxylene@gmail.com>
2020-07-09 15:32:38 +0200

    feature: swap as list element

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/collections/CollectionsExtensions.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/collections/CollectionsExtensions.kt
@@ -34,6 +34,14 @@ fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
     this[index2] = tmp
 }
 
+fun <T> MutableList<T>.swapAsList(index1: Int, index2: Int): MutableList<T> {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+    return this
+}
+
+
 /**
  * Gets mid element
  */
