f2380cd8d9cd40e181dc48a99bd71fa20643f572
CraZyLegenD <crazyxylene@gmail.com>
2020-08-05 12:09:26 +0200

    feature: any and kclass reflections

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/reflection/ReflectionUtils.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/reflection/ReflectionUtils.kt
@@ -384,6 +384,15 @@ inline fun <reified T : KClass<T>> T.isInner(): Boolean = this::class.isInner
 inline fun <reified T : KClass<T>> T.isOpen(): Boolean = this::class.isOpen
 
 
+inline fun <reified T : Any> T.isDataClass(): Boolean = this::class.isData
+inline fun <reified T : Any> T.isSealed(): Boolean = this::class.isSealed
+inline fun <reified T : Any> T.isAbstract(): Boolean = this::class.isAbstract
+inline fun <reified T : Any> T.isCompanion(): Boolean = this::class.isCompanion
+inline fun <reified T : Any> T.isFinal(): Boolean = this::class.isFinal
+inline fun <reified T : Any> T.isInner(): Boolean = this::class.isInner
+inline fun <reified T : Any> T.isOpen(): Boolean = this::class.isOpen
+
+
 inline fun <reified T : Any> T.isFinal(fieldName: String): Boolean? {
     return tryOrNull {
         this::class.java.getField(fieldName).kotlinProperty?.isFinal
