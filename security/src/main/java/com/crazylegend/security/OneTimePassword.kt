9488a18c93f5b8a4381412e2a430a7678d0c287e
CraZyLegenD <crazyxylene@gmail.com>
2021-01-04 11:03:38 +0100

    refactor: overloading the one time password generator function

--- security/src/main/java/com/crazylegend/security/OneTimePassword.kt
+++ security/src/main/java/com/crazylegend/security/OneTimePassword.kt
@@ -27,3 +27,23 @@ fun generateOneTimePassword(passwordLength: Int = 8, random: SecureRandom = Secu
     for (i in 0 until passwordLength) pass.append(chars[random.nextInt(passChars.length)])
     return pass.toString()
 }
+
+fun generateOneTimePassword(chars: CharArray = ("abcdefghijklmnopqrstuvwxyz"
+        + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        + "0123456789!@%$%&^?|~'\"#+="
+        + "\\*/.,:;[]()-_<>").toCharArray(), passwordLength: Int = 8, random: SecureRandom = SecureRandom(),
+                            shuffleCharacters: Boolean = true, exemptChars: List<Char> = emptyList()): String {
+
+    if (shuffleCharacters) {
+        chars.shuffle()
+    }
+
+    val finalChars = chars.toMutableList().also {
+        it.removeAll(exemptChars)
+    }
+    val passChars = String(finalChars.toCharArray())
+    val pass = StringBuilder()
+    for (i in 0 until passwordLength) pass.append(chars[random.nextInt(passChars.length)])
+    return pass.toString()
+}
+
