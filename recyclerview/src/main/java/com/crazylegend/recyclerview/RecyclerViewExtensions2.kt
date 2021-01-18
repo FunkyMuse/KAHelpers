13078108e2dc04e7c14d48ae8a17f9f12533f01c
CraZyLegenD <crazyxylene@gmail.com>
2021-01-18 12:19:32 +0100

    refactor: threshold has default value now

--- recyclerview/src/main/java/com/crazylegend/recyclerview/RecyclerViewExtensions2.kt
+++ recyclerview/src/main/java/com/crazylegend/recyclerview/RecyclerViewExtensions2.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
  * Created by hristijan on 1/17/21 to long live and prosper !
  */
 
-inline fun RecyclerView.hideOnScroll(threshold: Int,
+inline fun RecyclerView.hideOnScroll(threshold: Int = 20,
                                      crossinline hide: () -> Unit = {},
                                      crossinline show: () -> Unit = {}) {
     addOnScrollListener(object : HideOnScrollListener(threshold) {
