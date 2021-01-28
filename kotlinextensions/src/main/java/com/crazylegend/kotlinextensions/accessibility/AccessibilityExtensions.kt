9057aa87e7ad557c51a2d78b0fe7295771286756
CraZyLegenD <crazyxylene@gmail.com>
2021-01-28 14:02:39 +0100

    refactor: accessibility permission renamed and addition to highlight the service in the settings for stock OEMs

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/accessibility/AccessibilityExtensions.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/accessibility/AccessibilityExtensions.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.provider.Settings
 import android.text.TextUtils
 import android.view.accessibility.AccessibilityNodeInfo
+import androidx.core.os.bundleOf
 import com.crazylegend.kotlinextensions.context.accessibilityManager
 
 /**
@@ -40,8 +41,18 @@ inline fun <reified T : AccessibilityService> Context.isAccessibilityServiceRunn
     return settingsString != null && settingsString.contains("${packageName}/${T::class.java.name}")
 }
 
-fun Context.askForAccessibilityPermissions() = startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+fun Context.askForAccessibilityPermission() = startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
 
+inline fun <reified T> Context.askForAccessibilityPermissionHighlight(
+        argKey: String = ":settings:fragment_args_key",
+        showFragsKey: String = ":settings:show_fragment_args"
+) {
+    startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS).apply {
+        val showArgs = packageName + "/" + T::class.java.name
+        putExtra(argKey, showArgs)
+        putExtra(showFragsKey, bundleOf(argKey to showArgs))
+    })
+}
 
 fun logViewHierarchy(nodeInfo: AccessibilityNodeInfo, depth: Int = 0,
                      callback: (space: String, iteratedNode: AccessibilityNodeInfo) -> Unit) {
