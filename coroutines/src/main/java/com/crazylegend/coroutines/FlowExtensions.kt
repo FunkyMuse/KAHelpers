c87896dfa4892ef42f54481ce3fad67d96e1ec8c
CraZyLegenD <crazyxylene@gmail.com>
2021-01-28 14:30:32 +0100

    refactored the TextChanges function, now skip initial works correctly

--- coroutines/src/main/java/com/crazylegend/coroutines/FlowExtensions.kt
+++ coroutines/src/main/java/com/crazylegend/coroutines/FlowExtensions.kt
@@ -1,9 +1,11 @@
 package com.crazylegend.coroutines
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.TextView
-import androidx.core.widget.addTextChangedListener
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
 
@@ -45,20 +47,26 @@ fun <T> Flow<T>.ignoreFirst(): Flow<T> {
 
 fun TextView.textChanges(skipInitialValue: Boolean = false, debounce: Long = 300L): Flow<CharSequence?> =
         callbackFlow<CharSequence?> {
-            val listener = this@textChanges.addTextChangedListener {
-                if (!isClosedForSend)
-                    offer(it)
+            val listener = object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    offerIfNotClosed(s)
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
             }
+            this@textChanges.addTextChangedListener(listener)
             awaitClose {
                 removeTextChangedListener(listener)
             }
         }.buffer(Channel.CONFLATED)
-                .addSkipInitialValueOnClause(skipInitialValue)
+                .drop(dropInitialValueIfSkipped(skipInitialValue))
                 .debounce(debounce)
 
-fun <T> Flow<T>.addSkipInitialValueOnClause(skipInitialValue: Boolean): Flow<T> {
-    if (skipInitialValue) {
-        drop(1)
+private fun dropInitialValueIfSkipped(skipInitialValue: Boolean) = if (skipInitialValue) 1 else 0
+
+private fun <E> ProducerScope<E>.offerIfNotClosed(element: E) {
+    if (!isClosedForSend) {
+        offer(element)
     }
-    return this
 }
