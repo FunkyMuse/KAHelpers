73f238133f3c72aac51ecf938ebaa5648e9735ff
FunkyMuse <crazyxylene@gmail.com>
2021-02-24 10:44:40 +0100

    feat: accessible touch target

--- kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/views/ViewExtensions3.kt
+++ kotlinextensions/src/main/java/com/crazylegend/kotlinextensions/views/ViewExtensions3.kt
@@ -2,7 +2,10 @@ package com.crazylegend.kotlinextensions.views
 
 import android.animation.Animator
 import android.animation.ValueAnimator
+import android.content.Context
 import android.graphics.Rect
+import android.util.TypedValue
+import android.view.TouchDelegate
 import android.view.View
 import android.view.ViewAnimationUtils
 import androidx.annotation.ColorRes
@@ -122,3 +125,36 @@ fun View.createCircularReveal(
     }
     return animator
 }
+
+fun View.accessibleTouchTarget() {
+    post {
+        val delegateArea = Rect()
+        getHitRect(delegateArea)
+
+        // 48 dp is the minimum requirement. We need to convert this to pixels.
+        val accessibilityMin = context.dpToPx(48)
+
+        // Calculate size vertically, and adjust touch area if it's smaller then the minimum.
+        val height = delegateArea.bottom - delegateArea.top
+        if (accessibilityMin > height) {
+            // Add +1 px just in case min - height is odd and will be rounded down
+            val addition = ((accessibilityMin - height) / 2).toInt() + 1
+            delegateArea.top -= addition
+            delegateArea.bottom += addition
+        }
+
+        // Calculate size horizontally, and adjust touch area if it's smaller then the minimum.
+        val width = delegateArea.right - delegateArea.left
+        if (accessibilityMin > width) {
+            // Add +1 px just in case min - width is odd and will be rounded down
+            val addition = ((accessibilityMin - width) / 2).toInt() + 1
+            delegateArea.left -= addition
+            delegateArea.right += addition
+        }
+
+        val parentView = parent as? View
+        parentView?.touchDelegate = TouchDelegate(delegateArea, this)
+    }
+}
+
+fun Context.dpToPx(value: Int) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value.toFloat(), resources.displayMetrics)
