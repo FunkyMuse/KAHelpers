98d4007ef8e1d6d1b7cfa93e04bd480315327697
FunkyMuse <crazyxylene@gmail.com>
2021-08-12 00:47:04 +0200

    remove kotlin reflective from moshi

--- moshi/src/main/java/com/crazylegend/moshi/MoshiExtensions.kt
+++ moshi/src/main/java/com/crazylegend/moshi/MoshiExtensions.kt
@@ -1,9 +1,9 @@
 package com.crazylegend.moshi
 
 import android.content.SharedPreferences
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -13,51 +13,69 @@ import kotlin.reflect.KProperty
  */
 
 
-inline fun <reified T : Any> T.toJson(customBuilder: Moshi.Builder = Moshi.Builder()): String = customBuilder
-        .add(KotlinJsonAdapterFactory()).build()
-        .adapter<T>(T::class.java).toJson(this)
+inline fun <reified T : Any> T.toJson(
+    factory: JsonAdapter.Factory,
+    customBuilder: Moshi.Builder = Moshi.Builder()
+): String = customBuilder
+    .add(factory).build()
+    .adapter(T::class.java).toJson(this)
 
 
-inline fun <reified T : Any> String.fromJson(customBuilder: Moshi.Builder = Moshi.Builder()): T? = customBuilder
-        .add(KotlinJsonAdapterFactory()).build()
-        .adapter(T::class.java).fromJson(this)
+inline fun <reified T : Any> String.fromJson(
+    factory: JsonAdapter.Factory,
+    customBuilder: Moshi.Builder = Moshi.Builder()
+): T? = customBuilder
+    .add(factory).build()
+    .adapter(T::class.java).fromJson(this)
 
 
-inline fun <reified T : Any> String.toJsonObjectList(customBuilder: Moshi.Builder = Moshi.Builder()): List<T>? {
+inline fun <reified T : Any> String.toJsonObjectList(
+    factory: JsonAdapter.Factory,
+    customBuilder: Moshi.Builder = Moshi.Builder()
+): List<T>? {
     return Types.newParameterizedType(List::class.java, T::class.java).let { type ->
         customBuilder
-                .add(KotlinJsonAdapterFactory()).build()
-                .adapter<List<T>>(type).fromJson(this)
+            .add(factory).build()
+            .adapter<List<T>>(type).fromJson(this)
     }
 }
 
 
-inline fun <reified T : Any> String.toJsonObjectArrayList(customBuilder: Moshi.Builder = Moshi.Builder()): ArrayList<T>? {
+inline fun <reified T : Any> String.toJsonObjectArrayList(
+    factory: JsonAdapter.Factory,
+    customBuilder: Moshi.Builder = Moshi.Builder()
+): ArrayList<T>? {
     return Types.newParameterizedType(ArrayList::class.java, T::class.java).let { type ->
         customBuilder
-                .add(KotlinJsonAdapterFactory()).build()
-                .adapter<ArrayList<T>>(type).fromJson(this)
+            .add(factory).build()
+            .adapter<ArrayList<T>>(type).fromJson(this)
     }
 }
 
 
-inline fun <reified T : Any> String.toJsonObjectMutableList(customBuilder: Moshi.Builder = Moshi.Builder()): MutableList<T>? {
+inline fun <reified T : Any> String.toJsonObjectMutableList(
+    factory: JsonAdapter.Factory,
+    customBuilder: Moshi.Builder = Moshi.Builder()
+): MutableList<T>? {
     return Types.newParameterizedType(MutableList::class.java, T::class.java).let { type ->
         customBuilder
-                .add(KotlinJsonAdapterFactory()).build()
-                .adapter<MutableList<T>>(type).fromJson(this)
+            .add(factory).build()
+            .adapter<MutableList<T>>(type).fromJson(this)
     }
 }
 
 
-fun SharedPreferences.putObject(key: String, obj: Any?) {
-    putString(key, obj?.toJson() ?: "")
+fun SharedPreferences.putObject(factory: JsonAdapter.Factory, key: String, obj: Any?) {
+    putString(key, obj?.toJson(factory) ?: "")
 }
 
-inline fun <reified T : Any> SharedPreferences.getObject(key: String): T? {
+inline fun <reified T : Any> SharedPreferences.getObject(
+    factory: JsonAdapter.Factory,
+    key: String
+): T? {
     val string = getString(key, null)
     if (string == null || string.isEmpty()) return null
-    return getString(key, null)?.fromJson<T>()
+    return getString(key, null)?.fromJson(factory)
 }
 
 fun SharedPreferences.putString(key: String, value: String) {
@@ -65,8 +83,8 @@ fun SharedPreferences.putString(key: String, value: String) {
 }
 
 private inline fun SharedPreferences.edit(
-        commit: Boolean = false,
-        action: SharedPreferences.Editor.() -> Unit
+    commit: Boolean = false,
+    action: SharedPreferences.Editor.() -> Unit
 ) {
     val editor = edit()
     action(editor)
@@ -80,16 +98,18 @@ private inline fun SharedPreferences.edit(
 
 /**
  * Use sharedPreferences.obj( key = { "MY_KEY" } )
+ * @param factory JsonAdapter.Factory usually KotlinJsonAdapterFactory or your own choice
  * @receiver SharedPreferences
  * @param key Function1<KProperty<*>, String>
  * @return ReadWriteProperty<Any, T?>
  */
 inline fun <reified T> SharedPreferences.obj(
-        crossinline key: (KProperty<*>) -> String = KProperty<*>::name
+    factory: JsonAdapter.Factory,
+    crossinline key: (KProperty<*>) -> String = KProperty<*>::name
 ): ReadWriteProperty<Any, T?> = object : ReadWriteProperty<Any, T?> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): T? = getObject(key(property))
+    override fun getValue(thisRef: Any, property: KProperty<*>): T? =
+        getObject(factory, key(property))
 
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) = putObject(key(property), value)
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) =
+        putObject(factory, key(property), value)
 }
-
-val moshiReflective = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
