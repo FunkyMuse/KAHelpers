222404237946ec4a4278e9afd2ff7d0f9a575e9e
FunkyMuse <kikoski@protonmail.com>
2021-10-11 02:04:06 +0200

    feat: add sensible getters for the retrofit result

--- retrofit/src/main/java/com/crazylegend/retrofit/retrofitResult/RetrofitResultExtensions.kt
+++ retrofit/src/main/java/com/crazylegend/retrofit/retrofitResult/RetrofitResultExtensions.kt
@@ -15,7 +15,9 @@ fun <T> retrofitSuccess(value: T) = RetrofitResult.Success(value)
 val retrofitLoading get() = RetrofitResult.Loading
 val retrofitIdle get() = RetrofitResult.Loading
 
-fun <T> retrofitSubscribe(response: Response<T>): RetrofitResult<T> = response.unwrapResponseToModel()?.let { retrofitSuccess(it) } ?: retrofitApiError(response.code(), response.errorBody())
+fun <T> retrofitSubscribe(response: Response<T>): RetrofitResult<T> =
+    response.unwrapResponseToModel()?.let { retrofitSuccess(it) }
+        ?: retrofitApiError(response.code(), response.errorBody())
 
 inline fun <T> RetrofitResult<T>.onLoading(function: () -> Unit): RetrofitResult<T> {
     if (this is RetrofitResult.Loading) function()
@@ -57,3 +59,16 @@ fun <T> Response<T>.unwrapResponseToModel(): T? = when {
     isSuccessful -> body()
     else -> null
 }
+
+val <T> RetrofitResult<T>.isLoading get() = this is RetrofitResult.Loading
+val <T> RetrofitResult<T>.isSuccess get() = this is RetrofitResult.Success
+val <T> RetrofitResult<T>.isSuccessAndValueIsListAndNullOrEmpty get() = this is RetrofitResult.Success && isValueAListAndNullOrEmpty
+val <T> RetrofitResult<T>.isIdle get() = this is RetrofitResult.Idle
+val <T> RetrofitResult<T>.isApiError get() = this is RetrofitResult.ApiError
+val <T> RetrofitResult<T>.isError get() = this is RetrofitResult.Error
+
+
+val <T> RetrofitResult<T>.getSuccess: T? get() = if (this is RetrofitResult.Success) value else null
+val <T> RetrofitResult<T>.getThrowable: Throwable? get() = if (this is RetrofitResult.Error) throwable else null
+val <T> RetrofitResult<T>.getApiFailureCode: Int? get() = if (this is RetrofitResult.ApiError) responseCode else null
+val <T> RetrofitResult<T>.getApiResponseBody: ResponseBody? get() = if (this is RetrofitResult.ApiError) errorBody else null
