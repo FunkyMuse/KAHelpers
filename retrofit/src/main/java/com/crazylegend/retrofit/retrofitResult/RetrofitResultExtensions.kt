7bd42ec7c5eef622c6d90c3e5cb0b34749102bc0
FunkyMuse <kikoski@protonmail.com>
2021-11-16 15:35:02 +0100

    add some new retrofit result extensions

--- retrofit/src/main/java/com/crazylegend/retrofit/retrofitResult/RetrofitResultExtensions.kt
+++ retrofit/src/main/java/com/crazylegend/retrofit/retrofitResult/RetrofitResultExtensions.kt
@@ -8,17 +8,18 @@ import retrofit2.Response
  * Created by crazy on 6/15/20 to long live and prosper !
  */
 
-//region State
 fun retrofitCallError(throwable: Throwable) = RetrofitResult.Error(throwable)
 fun retrofitApiError(code: Int, errorBody: ResponseBody?) = RetrofitResult.ApiError(code, errorBody)
 fun <T> retrofitSuccess(value: T) = RetrofitResult.Success(value)
 val retrofitLoading get() = RetrofitResult.Loading
-val retrofitIdle get() = RetrofitResult.Loading
+val retrofitIdle get() = RetrofitResult.Idle
 
 fun <T> retrofitSubscribe(response: Response<T>): RetrofitResult<T> =
     response.unwrapResponseToModel()?.let { retrofitSuccess(it) }
         ?: retrofitApiError(response.code(), response.errorBody())
 
+
+//region State
 inline fun <T> RetrofitResult<T>.onLoading(function: () -> Unit): RetrofitResult<T> {
     if (this is RetrofitResult.Loading) function()
 
@@ -53,6 +54,47 @@ inline fun <T> RetrofitResult<T>.onSuccess(function: (model: T) -> Unit): Retrof
     return this
 }
 //endregion
+
+
+//region State suspend
+suspend fun <T> RetrofitResult<T>.onLoadingSuspend(function: suspend () -> Unit): RetrofitResult<T> {
+    if (this is RetrofitResult.Loading) function()
+
+    return this
+}
+
+suspend fun <T> RetrofitResult<T>.onIdleSuspend(function: suspend () -> Unit): RetrofitResult<T> {
+    if (this is RetrofitResult.Idle) function()
+
+    return this
+}
+
+suspend fun <T> RetrofitResult<T>.onErrorSuspend(function: suspend (throwable: Throwable) -> Unit): RetrofitResult<T> {
+    if (this is RetrofitResult.Error) {
+        function(throwable)
+    }
+    return this
+}
+
+suspend fun <T> RetrofitResult<T>.onApiErrorSuspend(function: suspend (errorBody: ResponseBody?, responseCode: Int) -> Unit): RetrofitResult<T> {
+    if (this is RetrofitResult.ApiError) {
+        function(errorBody, responseCode)
+    }
+
+    return this
+}
+
+suspend fun <T> RetrofitResult<T>.onSuccessSuspend(function: suspend (model: T) -> Unit): RetrofitResult<T> {
+    if (this is RetrofitResult.Success) {
+        function(value)
+    }
+    return this
+}
+//endregion
+
+
+fun <VALUE, TRANSFORM> RetrofitResult<VALUE>.transform(transformer: (VALUE) -> TRANSFORM) :TRANSFORM? =
+    getSuccess?.let { value -> transformer(value) }
 
 
 fun <T> Response<T>.unwrapResponseToModel(): T? = when {
