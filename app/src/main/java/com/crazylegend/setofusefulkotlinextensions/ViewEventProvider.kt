f4a72205b771a6bfd99c93c7e3f2e34855aac0b4
FunkyMuse <kikoski@protonmail.com>
2022-04-27 21:59:49 +0200

    feat: add exactly once event bus and how to provide events immediately to the ui

--- app/src/main/java/com/crazylegend/setofusefulkotlinextensions/ViewEventProvider.kt
+++ app/src/main/java/com/crazylegend/setofusefulkotlinextensions/ViewEventProvider.kt
@@ -2,17 +2,19 @@ package com.crazylegend.setofusefulkotlinextensions
 
 import com.crazylegend.retrofit.viewstate.event.ViewEvent
 import com.crazylegend.retrofit.viewstate.event.ViewEventContract
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.withContext
 
 //should be injected
 class ViewEventProvider : ViewEventContract {
 
-    private val channelEvents : Channel<ViewEvent> = Channel(Channel.BUFFERED)
+    private val channelEvents: Channel<ViewEvent> = Channel(Channel.BUFFERED)
     override val viewEvent: Flow<ViewEvent> = channelEvents.receiveAsFlow()
 
-    override suspend fun provideEvent(viewEvent: ViewEvent) {
+    override suspend fun provideEvent(viewEvent: ViewEvent) = withContext(Dispatchers.Main.immediate) {
         channelEvents.send(viewEvent)
     }
 }
