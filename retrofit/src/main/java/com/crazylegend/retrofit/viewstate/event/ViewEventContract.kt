0d5404e3d3730689af21e506e0881632f0825fea
FunkyMuse <kikoski@protonmail.com>
2022-04-27 22:24:14 +0200

    refactor: remove dead code

--- retrofit/src/main/java/com/crazylegend/retrofit/viewstate/event/ViewEventContract.kt
+++ retrofit/src/main/java/com/crazylegend/retrofit/viewstate/event/ViewEventContract.kt
@@ -3,6 +3,7 @@ package com.crazylegend.retrofit.viewstate.event
 import kotlinx.coroutines.flow.Flow
 
 interface ViewEventContract {
+    //decouple this as well so that a direct flow is not forced, hmm
     val viewEvent: Flow<ViewEvent>
 
     suspend fun provideEvent(viewEvent: ViewEvent)
