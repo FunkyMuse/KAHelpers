a7cdd9f7625d8ab04574928db34d981ab90d5314
5peak2me <461260911@qq.com>
2023-07-13 11:10:05 +0800

    refactor: remove some inspection warnings in module common

--- common/src/main/java/com/crazylegend/common/BasicExtensions.kt
+++ common/src/main/java/com/crazylegend/common/BasicExtensions.kt
@@ -24,10 +24,11 @@ fun <T : Any> T?.or(item: T?) =
 
 
 fun <T : CharSequence> T?.orElse(item: T) =
-        if (this != null && isNotBlank()) this else item
+        if (!isNullOrBlank()) this else item
 
 fun <T : CharSequence> T?.or(item: T?) =
-        if (this != null && isNotBlank()) this else item
+        if (!isNullOrBlank()) this else item
+
 
 fun <T : Number> T?.orElse(number: T) =
         if (this != null && this != 0) this else number
@@ -35,18 +36,19 @@ fun <T : Number> T?.orElse(number: T) =
 fun <T : Number> T?.or(number: T?) =
         if (this != null && this == 0) this else number
 
+
 fun <T> List<T>?.orElse(list: List<T>) =
-        if (this != null && isNotEmpty()) this else list
+        if (!isNullOrEmpty()) this else list
 
 fun <T> List<T>?.or(list: List<T>?) =
-        if (this != null && isNotEmpty()) this else list
+        if (!isNullOrEmpty()) this else list
 
 
 fun <K, V> Map<K, V>?.orElse(map: Map<K, V>) =
-        if (this != null && isNotEmpty()) this else map
+        if (!isNullOrEmpty()) this else map
 
 fun <K, V> Map<K, V>?.or(map: Map<K, V>?) =
-        if (this != null && isNotEmpty()) this else map
+        if (!isNullOrEmpty()) this else map
 
 inline fun infiniteLoop(action: () -> Unit) {
     while (true) {
