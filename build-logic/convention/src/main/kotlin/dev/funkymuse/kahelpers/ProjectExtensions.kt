10cba8f6cc6a383d235b832846916a2c272b7505
FunkyMuse <kikoski@protonmail.com>
2024-05-22 11:11:24 +0200

    optimize imports

--- build-logic/convention/src/main/kotlin/dev/funkymuse/kahelpers/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/dev/funkymuse/kahelpers/ProjectExtensions.kt
@@ -10,7 +10,6 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun CommonExtension<*, *, *, *, *, *>.configureBuildFeatures() {
     buildFeatures.apply {
